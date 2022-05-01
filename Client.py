6391cf5f431b3cddd07afd20d678f5a8134316ff
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2022-05-01 17:20:20 +0900

    tcp 통신 방법으로 client측 재구현

--- Client.py
+++ Client.py
@@ -5,10 +5,13 @@ PORT = 3091
 
 ADDRESS = (IP, PORT)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as CLIENT:
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as CLIENT: # 클라이언트 소켓을 만든 뒤, CLIENT로 지칭
     CLIENT.connect(ADDRESS)
-    while True:
-        MESSAGE = input("<< ").encode()
-        CLIENT.send(MESSAGE)
-        DATA = CLIENT.recv(1024)
-        print(">>", DATA.decode())
+
+    MESSAGE = input("<< ").encode() # 클라이언트가 서버에 전송할 메시지 입력 (input)
+    CLIENT.send(MESSAGE) # 메시지 전송 (클라이언트 -> 서버)
+
+    DATA = CLIENT.recv(1024) # 데이터 수신 (클라이언트 <- 서버)
+    print(">>", DATA.decode()) # decode 마친 후 출력
+
+    CLIENT.close() # 소켓 닫기
--- Server.py
+++ Server.py
@@ -1,10 +1,14 @@
+from ast import Try
 import socket
 import requests
 
-HOST = "127.0.0.1" # 127.0.0.1 == localhost == 내 컴퓨터
+
+HOST = "127.0.0.1" # localhost == 내 컴퓨터
 PORT = 3091
 
 ADDRESS = (HOST, PORT)
+
+print(ADDRESS)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as SERVER:
     
@@ -20,7 +24,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as SERVER:
         
         if DATA.decode() == "q":
             print("Quit connection")
-            exit()
+            break
         
         FUNC, URL = DATA.decode().split()
         JSON = {"outer": {"inner": "value"}}
@@ -28,22 +32,59 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as SERVER:
 
         print("client >> ", DATA.decode())
 
+        # Python3의 각 명령어 수행 뒤, 파일 입출력을 사용하기 위해 변수 설정
+        fileName = FUNC + "_" + URL.replace("http://", "") + ".txt" 
 
         if FUNC == "GET":
             RESPONSE = requests.get(URL)
         
+            try:
+                f = open(fileName, 'w')
+                print('success - w')
+            except:
+                f = open(fileName, 'x')
+                print('success - x')
+            f.write(str(RESPONSE))
+            f.close()
+
         elif FUNC == "HEAD":
             RESPONSE = requests.head(URL)
         
+            try:
+                f = open(fileName, 'w')
+                print('success - w')
+            except:
+                f = open(fileName, 'x')
+                print('success - x')
+            f.write(str(RESPONSE))
+            f.close()
+
         elif FUNC == "POST":
             RESPONSE = requests.post(URL, data = JSON)
+
+            try:
+                f = open(fileName, 'w')
+                print('success - w')
+            except:
+                f = open(fileName, 'x')
+                print('success - x')
+            f.write(str(RESPONSE))
+            f.close()
 
         elif FUNC == "PUT":
             RESPONSE = requests.put(URL, data = JSON)
 
+            try:
+                f = open(fileName, 'w')
+                print('success - w')
+            except:
+                f = open(fileName, 'x')
+                print('success - x')
+            f.write(str(RESPONSE))
+            f.close()
+
         else:
             RESPONSE = "usage: GET/HEAD/POST/PUT (ADDRESS)"
-
 
         if RESPONSE.status_code == 200:
             MESSAGE = "200 OK"
